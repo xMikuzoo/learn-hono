b38c8523e31efadd0898df55c15ef0e71aac3b44
Wojciech Król <105241268+xMikuzoo@users.noreply.github.com>
2026-04-11 17:56:19 +0200

    feat: enhance API routes with JWT authentication setup

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,21 @@
 import { Hono } from 'hono'
+import { jwt, type JwtVariables } from 'hono/jwt'
+
+import { env } from '@/env'
 
 import notesApp from './notes'
 import usersApp from './users'
 
-const api = new Hono()
+const api = new Hono<{
+  Variables: JwtVariables
+}>()
+
+api.use(
+  jwt({
+    secret: env.JWT_SECRET,
+    alg: 'HS256',
+  }),
+)
 
 api.route('/notes', notesApp)
 api.route('/users', usersApp)
